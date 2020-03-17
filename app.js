03372a9342ee359d12889e24fd9fb3131843e5c2
abehes <abehesn@calstatela.edu>
2020-03-17 02:41:37 -0700

    Added cross-env for multi-platform NODE_ENV support

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const fs = require("fs");
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 //MIDDLE WARE
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the MIDDLEWARE');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
