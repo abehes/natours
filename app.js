5be578e980d00711f61062eb544bfce3a3d19545
abehes <abehesn@calstatela.edu>
2020-03-16 18:16:35 -0700

    Refactored API routing and made CRUD functions more easy to read.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
-
 const express = require("express");
+
 
 const app = express();
 
+//MIDDLE WARE
 app.use(express.json());
+
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
@@ -18,9 +20,9 @@ app.get("/api/v1/tours", (req, res) => {
       tours
     }
   });
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTourById = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find(el => el.id === id);
@@ -38,9 +40,9 @@ app.get("/api/v1/tours/:id", (req, res) => {
       tour
     }
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -57,9 +59,9 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(400).json({
       status: "fail",
@@ -73,7 +75,33 @@ app.patch("/api/v1/tours/:id", (req, res) => {
       tour: "<Updated tour here..."
     }
   });
-});
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Invalid ID"
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null
+  });
+};
+
+
+app
+  .route("/api/v1/tours")
+    .get(getAllTours)
+    .post(createTour);
+    
+app
+  .route("/api/v1/tours/:id")
+    .get(getTourById)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
