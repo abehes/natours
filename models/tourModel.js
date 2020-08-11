fb9b7a4fa8413ce16b27cc332876456f8c23e50c
abehes <abehesn@calstatela.edu>
2020-08-10 19:26:07 -0700

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -72,9 +74,17 @@ tourSchema.virtual('durationWeeks').get(function() {
 });
 
 //Document Middleware
-tourSchema.pre('save', function() {
-  console.log(this);
+//You can have multiple middlewares for a hook, {hook = save for example}
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true});
+  next();
 });
+
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
